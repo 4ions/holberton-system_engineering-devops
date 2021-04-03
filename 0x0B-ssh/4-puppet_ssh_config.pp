492a6de08426a9a79ac0376d549bc216065e461d
4ions <leovalenciasantana@gmail.com>
2021-04-03 15:28:48 -0500

    Testing

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# client configuration with puppet
+file_line { 'Turn off Authentication':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'IdentityFile':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
+}
