3ccd49da9b934a4f94ec7629cf8e2cd8b33bad13
4ions <leovalenciasantana@gmail.com>
2021-04-12 16:01:57 -0500

    With puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,5 +2,10 @@
 exec {'/usr/bin/env apt-get -y update': }
 exec {'/usr/bin/env apt-get -y install nginx': }
 exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html': }
+-> file_line { 'add header' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
 exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
 exec {'/usr/bin/env service nginx start': }
