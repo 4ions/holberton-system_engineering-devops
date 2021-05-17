e34c30538ca691883425af2f914b6ac59adcd524
4ions <leovalenciasantana@gmail.com>
2021-05-17 16:09:15 -0500

    Subs in reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" Reddit API """ 
+""" Reddit API """
 import requests
+
 
 def number_of_subscribers(subreddit):
     """ Number of subscribers """
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get('https://reddit.com/r/{}/about.json'.format(subreddit), headers=headers)
+    response = requests.get('https://reddit.com/r/{}/about.json'
+                            .format(subreddit), headers=headers)
 
     if response.status_code != 200:
         return 0
-    return response.json().get('data', {}).get('subscribers',0)
+    return response.json().get('data', {}).get('subscribers', 0)
